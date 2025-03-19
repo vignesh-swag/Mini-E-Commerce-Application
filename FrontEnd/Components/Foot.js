6cea2f1df2639f71288d36ea8de6499afafb9fe9
vignesh-swag <163710693+vignesh-swag@users.noreply.github.com>
2025-03-19 21:20:56 +0530

    Add files via upload

---
+++ FrontEnd/Components/Foot.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Foot() {
+  return (
+    
+        <footer className="py-1 bg-dark">
+          <p className="text-center text-white mt-1 ">
+            JVLcart - 2023-2024, All Rights Reserved
+          </p>
+        </footer>
+  )
+}
+
+export default Foot
